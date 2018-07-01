2d44b1dc7d50b0c816ab8582ebd375fa0688632b
piekario13 <piekario13@gmail.com>
2018-07-01 21:23:32 +0200

    joke

---
+++ script.js
@@ -0,0 +1,17 @@
+let url = 'http://api.icndb.com/jokes/random';
+let paragraph = document.getElementById('joke');
+
+let button = document.getElementById('get-joke');
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+function getJoke() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    let response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
